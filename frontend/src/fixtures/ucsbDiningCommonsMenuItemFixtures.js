6415f32543503340a13962520bb7f37a9475fbfb
Emily Zheng <61632678+emiz39@users.noreply.github.com>
2024-11-02 19:46:52 -0700

    create fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneDate: {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "Chicken Caesar Salad",
+      station: "Entree",
+    },
+    threeDates: [
+      {
+        id: 1,
+        diningCommonsCode: "ortega",
+        name: "Chicken Caesar Salad",
+        station: "Entree0",
+      },
+      {
+        id: 2,
+        diningCommonsCode: "portola",
+        name: "Baked Pesto Pasta with Chicken",
+        station: "Entree Specials",
+      },
+      {
+        id: 3,
+        diningCommonsCode: "ortega",
+        name: "Baked Pesto Pasta with Chicken",
+        station: "Entree Specials",
+      },
+    ],
+  };
+  
+  export { ucsbDiningCommonsMenuItemFixtures };
+  

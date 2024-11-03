4f123b806284648760fad7999562a6383f05e074
Andy Subramanian <andysubramanian@Andys-MacBook-Pro.local>
2024-11-02 20:24:38 -0700

    Update ArticlesController and add articles fixtures

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,5 +1,14 @@
 const articlesFixtures = {
-    oneArticle: {
+  oneArticle: {
+    id: 1,
+    title: "Andys Article",
+    url: "example.com",
+    explanation: "Article Number 1",
+    email: "author@example.com",
+    dateAdded: "2022-01-01T12:00:00",
+  },
+  threeArticles: [
+    {
       id: 1,
       title: "Andys Article",
       url: "example.com",
@@ -7,33 +16,23 @@ const articlesFixtures = {
       email: "author@example.com",
       dateAdded: "2022-01-01T12:00:00",
     },
-    threeArticles: [
-      {
-        id: 1,
-        title: "Andys Article",
-        url: "example.com",
-        explanation: "Article Number 1",
-        email: "author@example.com",
-        dateAdded: "2022-01-01T12:00:00",
-      },
-      {
-        id: 2,
-        title: "Andys 2nd Article",
-        url: "example.com",
-        explanation: "Article Number 2",
-        email: "author@example.com",
-        dateAdded: "2022-01-01T12:00:00",
-      },
-      {
-        id: 3,
-        title: "Andys 3rd Article",
-        url: "example.com",
-        explanation: "Article Number 3",
-        email: "author@example.com",
-        dateAdded: "2022-01-01T12:00:00",
-      },
-    ],
-  };
-  
-  export { articlesFixtures };
-  
+    {
+      id: 2,
+      title: "Andys 2nd Article",
+      url: "example.com",
+      explanation: "Article Number 2",
+      email: "author@example.com",
+      dateAdded: "2022-01-01T12:00:00",
+    },
+    {
+      id: 3,
+      title: "Andys 3rd Article",
+      url: "example.com",
+      explanation: "Article Number 3",
+      email: "author@example.com",
+      dateAdded: "2022-01-01T12:00:00",
+    },
+  ],
+};
+
+export { articlesFixtures };

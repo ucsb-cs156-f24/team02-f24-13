15b0b7be8fc10a201c5f08c3d7b2f30656311456
Shivani Madhan <madhan.shivani1104@gmail.com>
2024-11-05 15:59:10 -0800

    added fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,36 @@
+const ucsbOrganizationFixtures = {
+    oneOrganization: [
+      {
+        orgCode: "zpr",
+        orgTranslationShort: "Zeta Phi",
+        orgTranslation: "Zeta Phi Rho",
+        inactive: false
+      },
+    ],
+  
+    threeOrganizations: [
+      {
+        orgCode: "cdt",
+        orgTranslationShort: "Chi Delts",
+        orgTranslation: "Chi Delta Theta",
+        inactive: false
+      },
+  
+      {
+        orgCode: "zbt",
+        orgTranslationShort: "Zeta Beta",
+        orgTranslation: "Zeta Beta Tau",
+        inactive: false
+      },
+  
+      {
+        orgCode: "slb",
+        orgTranslationShort: "Sig Lam",
+        orgTranslation: "Sigma Lambda Beta",
+        inactive: false
+      },
+    ],
+  };
+  
+  export { ucsbOrganizationFixtures };
+  

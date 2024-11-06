e456968323175e9ed42bf9b387a8611757fc7fcb
Anvitha Kosuri <anvithakosuri@ucsb.edu>
2024-11-05 23:42:06 -0800

    fin

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,43 +1,42 @@
 const helpRequestFixtures = {
-    firstRequest: {
+  firstRequest: {
+    id: 13,
+    requesterEmail: "dude101@gmail.com",
+    teamId: "t-13",
+    tableOrBreakoutRoom: "13",
+    requestTime: "2022-01-02T12:00:00",
+    explanation: "check frontend",
+    solved: false,
+  },
+  otherRequests: [
+    {
       id: 13,
-      requesterEmail: "dude101@gmail.com",
-      teamId: "t-13",
+      requesterEmail: "bob@gmail.com",
+      teamId: "t-2",
       tableOrBreakoutRoom: "13",
-      requestTime: "2022-01-02T12:00:00",
-      explanation: "check frontend",
+      requestTime: "2022-01-02T12:30:00",
+      explanation: "not working",
       solved: false,
     },
-    otherRequests: [
-      {
-        id: 13,
-        requesterEmail: "bob@gmail.com",
-        teamId: "t-2",
-        tableOrBreakoutRoom: "13",
-        requestTime: "2022-01-02T12:00:00",
-        explanation: "not working",
-        solved: false,
-      },
-      {
-        id: 22,
-        requesterEmail: "steve@gmail.com",
-        teamId: "t-1",
-        tableOrBreakoutRoom: "5",
-        requestTime: "2022-04-03T14:30:00",
-        explanation: "false",
-        solved: true,
-      },
-      {
-        id: 35,
-        requesterEmail: "imposter@gmail.com",
-        teamId: "t-3",
-        tableOrBreakoutRoom: "8",
-        requestTime: "2022-07-04T16:45:00",
-        explanation: "incorrect",
-        solved: false,
-      },
-    ],
-  };
-  
-  export {helpRequestFixtures};
-  
+    {
+      id: 22,
+      requesterEmail: "steve@gmail.com",
+      teamId: "t-1",
+      tableOrBreakoutRoom: "5",
+      requestTime: "2022-04-03T14:37:00",
+      explanation: "false",
+      solved: true,
+    },
+    {
+      id: 35,
+      requesterEmail: "imposter@gmail.com",
+      teamId: "t-3",
+      tableOrBreakoutRoom: "8",
+      requestTime: "2022-07-04T09:98:00",
+      explanation: "incorrect",
+      solved: false,
+    },
+  ],
+};
+
+export { helpRequestFixtures };

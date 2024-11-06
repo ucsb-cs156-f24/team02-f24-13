68360cf862b2295ad68d2251be2b8a650b9d26dd
Emily Zheng <61632678+emiz39@users.noreply.github.com>
2024-11-05 20:53:18 -0800

    updates to create page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -77,7 +77,7 @@ function App() {
           <>
             <Route
               exact
-              path="/diningcommonsmenuitem"
+              path="/ucsbdiningcommonsmenuitem"
               element={<UCSBDiningCommonsMenuItemIndexPage />}
             />
           </>
@@ -86,12 +86,12 @@ function App() {
           <>
             <Route
               exact
-              path="/diningcommonsmenuitem/edit/:id"
+              path="/ucsbdiningcommonsmenuitem/edit/:id"
               element={<UCSBDiningCommonsMenuItemEditPage />}
             />
             <Route
               exact
-              path="/diningcommonsmenuitem/create"
+              path="/ucsbdiningcommonsmenuitem/create"
               element={<UCSBDiningCommonsMenuItemCreatePage />}
             />
           </>

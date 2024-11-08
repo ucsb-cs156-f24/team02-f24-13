338a492696e83c4ac30ee346bc76f6b5bce740e5
David Qiao <dqiao@ucsb.edu>
2024-11-07 17:27:54 -0800

    grammar errors

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -69,7 +69,6 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/menuitemreview">
                     Menu Item Review
                   </Nav.Link>
-                
                   <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
                     UCSB Dining Commons Menu Item
                   </Nav.Link>

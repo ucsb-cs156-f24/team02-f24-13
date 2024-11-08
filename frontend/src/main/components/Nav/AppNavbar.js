d35548e4cd578c5b123fbc0fdeb7e07d0df9ce53
David Qiao <dqiao@ucsb.edu>
2024-11-07 17:42:31 -0800

    lmao what is this

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -75,7 +75,6 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/menuitemreview">
                     Menu Item Review
                   </Nav.Link>
-                  
                 </>
               ) : (
                 <></>

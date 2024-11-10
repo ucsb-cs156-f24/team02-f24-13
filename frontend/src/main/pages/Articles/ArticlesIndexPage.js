1e43599c87abacd310e6fa87a77dbb2cb819793f
Andy Subramanian <andysubramanian@169-231-144-213.wireless.ucsb.edu>
2024-11-09 20:55:18 -0800

    AS-Articles-Edit: Modified ArticlesIndexPage, updated test, and added new story for Articles page

--- frontend/src/main/pages/Articles/ArticlesIndexPage.js
+++ frontend/src/main/pages/Articles/ArticlesIndexPage.js
@@ -1,16 +1,45 @@
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import ArticlesTable from "main/components/Articles/ArticlesTable";
+import { Button } from "react-bootstrap";
+import { useCurrentUser, hasRole } from "main/utils/currentUser";
 
 export default function ArticlesIndexPage() {
+  const currentUser = useCurrentUser();
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+      return (
+        <Button
+          variant="primary"
+          href="/articles/create"
+          style={{ float: "right" }}
+          data-testid="ArticlesIndexPage-create-button"
+        >
+          Create Article
+        </Button>
+      );
+    }
+  };
+
+  const {
+    data: articles,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    ["/api/articles/all"],
+    { method: "GET", url: "/api/articles/all" },
+    [],
+  );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/articles/create">Create</a>
-        </p>
-        <p>
-          <a href="/articles/edit/1">Edit</a>
-        </p>
+        {createButton()}
+        <h1>Articles</h1>
+        <ArticlesTable articles={articles} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );

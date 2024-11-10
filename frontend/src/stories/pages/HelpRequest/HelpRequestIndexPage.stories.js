b0d213ba9d562641f6db2611ea263ed092c09bbd
Anvitha Kosuri <anvithakosuri@ucsb.edu>
2024-11-10 14:59:02 -0800

    ran prettier on indexPageHelpRequest

--- frontend/src/stories/pages/HelpRequest/HelpRequestIndexPage.stories.js
+++ frontend/src/stories/pages/HelpRequest/HelpRequestIndexPage.stories.js
@@ -7,62 +7,62 @@ import { http, HttpResponse } from "msw";
 import HelpRequestIndexPage from "main/pages/HelpRequest/HelpRequestIndexPage";
 
 export default {
-    title: "pages/HelpRequest/HelpRequestIndexPage",
-    component: HelpRequestIndexPage,
+  title: "pages/HelpRequest/HelpRequestIndexPage",
+  component: HelpRequestIndexPage,
 };
 
 const Template = () => <HelpRequestIndexPage storybook={true} />;
 
 export const Empty = Template.bind({});
 Empty.parameters = {
-    msw: [
-        http.get("/api/currentUser", () => {
-            return HttpResponse.json(apiCurrentUserFixtures.userOnly, {
-                status: 200,
-            });
-        }),
-        http.get("/api/systemInfo", () => {
-            return HttpResponse.json(systemInfoFixtures.showingNeither, {
-                status: 200,
-            });
-        }),
-        http.get("/api/helprequests/all", () => {
-            return HttpResponse.json([], { status: 200 });
-        }),
-    ],
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.userOnly, {
+        status: 200,
+      });
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither, {
+        status: 200,
+      });
+    }),
+    http.get("/api/helprequests/all", () => {
+      return HttpResponse.json([], { status: 200 });
+    }),
+  ],
 };
 
 export const ThreeItemsOrdinaryUser = Template.bind({});
 
 ThreeItemsOrdinaryUser.parameters = {
-    msw: [
-        http.get("/api/currentUser", () => {
-            return HttpResponse.json(apiCurrentUserFixtures.userOnly);
-        }),
-        http.get("/api/systemInfo", () => {
-            return HttpResponse.json(systemInfoFixtures.showingNeither);
-        }),
-        http.get("/api/helprequests/all", () => {
-            return HttpResponse.json(helpRequestFixtures.threeRequests);
-        }),
-    ],
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.userOnly);
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither);
+    }),
+    http.get("/api/helprequests/all", () => {
+      return HttpResponse.json(helpRequestFixtures.threeRequests);
+    }),
+  ],
 };
 
 export const ThreeItemsAdminUser = Template.bind({});
 
 ThreeItemsAdminUser.parameters = {
-    msw: [
-        http.get("/api/currentUser", () => {
-            return HttpResponse.json(apiCurrentUserFixtures.adminUser);
-        }),
-        http.get("/api/systemInfo", () => {
-            return HttpResponse.json(systemInfoFixtures.showingNeither);
-        }),
-        http.get("/api/helprequests/all", () => {
-            return HttpResponse.json(helpRequestFixtures.threeRequests);
-        }),
-        http.delete("/api/helprequests", () => {
-            return HttpResponse.json({}, { status: 200 });
-        }),
-    ],
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.adminUser);
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither);
+    }),
+    http.get("/api/helprequests/all", () => {
+      return HttpResponse.json(helpRequestFixtures.threeRequests);
+    }),
+    http.delete("/api/helprequests", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
 };

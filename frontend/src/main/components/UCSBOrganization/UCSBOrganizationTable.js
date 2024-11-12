7344f92a858c189064c7ea9ceacd007a423d014d
Shivani Madhan <madhan.shivani1104@gmail.com>
2024-11-11 17:47:18 -0800

    making changes for table component

---
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
@@ -0,0 +1,68 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import {
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
+} from "main/utils/UCSBOrganizationUtils";
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+export default function UCSBOrganizationTable({
+  organizations,
+  currentUser,
+  testIdPrefix = "UCSBOrganizationTable",
+}) {
+  const navigate = useNavigate();
+
+  const editCallback = (cell) => {
+    navigate(`/ucsbOrganization/edit/${cell.row.values.orgCode}`);
+  };
+
+  // Stryker disable all : hard to test for query caching
+
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    ["/api/ucsbOrganization/all"],
+  );
+  // Stryker restore all
+
+  // Stryker disable next-line all : TODO try to make a good test for this
+  const deleteCallback = async (cell) => {
+    const orgId = cell.row.values.orgCode; // or the correct identifier
+    deleteMutation.mutate({id: orgId});
+  };
+
+  const columns = [
+    {
+      Header: "Org Code",
+      accessor: "orgCode", // accessor is the "key" in the data
+    },
+
+    {
+      Header: "Org Translation Short",
+      accessor: "orgTranslationShort",
+    },
+    {
+      Header: "Org Translation",
+      accessor: "orgTranslation",
+    },
+    {
+      Header: "Inactive",
+      accessor: (organizations) => organizations.inactive.toString(),
+    }
+  ];
+
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
+    columns.push(ButtonColumn("Edit", "primary", editCallback, testIdPrefix));
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, testIdPrefix),
+    );
+  }
+
+  return (
+    <OurTable data={organizations} columns={columns} testid={testIdPrefix} />
+  );
+}

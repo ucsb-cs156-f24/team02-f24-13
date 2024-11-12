000350820aec891f0b559a5860dbfe961ab5027e
Shivani Madhan <madhan.shivani1104@gmail.com>
2024-11-12 11:08:18 -0800

    edit page fix

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -66,21 +66,8 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
-                    UCSB Dining Commons Menu Item
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/recommendationrequest">
-                    Recommendation Request
-                  </Nav.Link>
-
-                  <Nav.Link as={Link} to="/helprequests">
-                    Help Request
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/articles">
-                    Articles
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/menuitemreview">
-                    Menu Item Review
+                  <Nav.Link as={Link} to="/placeholder">
+                    Placeholder
                   </Nav.Link>
                   <Nav.Link as={Link} to="/ucsborganization">
                     UCSBOrganization
